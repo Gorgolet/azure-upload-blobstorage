8ddbd38ddc021ccbb15cba3e80791f05aaf045f2
Clément Roche <clement.roche@energisme.com>
2020-04-15 11:18:11 +0200

    add link element in unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Gorgo link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Gorgo/i);
   expect(linkElement).toBeInTheDocument();
 });
